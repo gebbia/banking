cfb3c22cd2328e26229cd3df9d4333eecb0ba3f8
Natasha Gebbia <gebbianatasha@gmail.com>
2024-08-12 04:00:50 -0500

    implement doughnut chart

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import HeaderBox from '@/components/ui/HeaderBox';
+import TotalBalanceBox from '@/components/ui/TotalBalanceBox';
 
 const Home = () => {
+    const loggedIn = {firstName: 'Natasha' };
+
     return (
-        <div>Dashboard</div>
+        <section className="home">
+            <div className="home-content">
+                <header className="home-header">
+                    <HeaderBox 
+                        type="greeting"
+                        title="Welcome"
+                        user={loggedIn?.firstName || 'Guest'}
+                        subtext="Access and manage your accounts and transactions efficiently."
+                    />
+
+                    <TotalBalanceBox
+                        accounts={[]}
+                        totalBanks={1}
+                        totalCurrentBalance={1250.35}
+                    />
+                 </header>
+            </div>
+        </section>
     )
 }
 
